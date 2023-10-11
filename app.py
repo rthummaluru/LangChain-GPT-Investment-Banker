436b688dc8068e2b32904ec3358fbf66a8bc8c7f
rthummaluru <120275888+rthummaluru@users.noreply.github.com>
2023-10-11 17:25:30 -0400

    updated file and added pdf to read

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ from langchain.llms import OpenAI
 import streamlit as st 
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
+from apikey import apikey
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+# Import vector store stuff
+from langchain.agents.agent_toolkits import(
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo
+)
 
 
 # import API KEY
-os.environ['OPENAI_API_KEY'] = 'apikey'
+os.environ['OPENAI_API_KEY'] = apikey
 
 llm = OpenAI(temperature=0.9)
 
@@ -14,14 +23,38 @@ llm = OpenAI(temperature=0.9)
 prompt = st.text_input('Input your prompt here')
 
 # Create and load pdf loader
-loader = PyPDFLoader('FullTime_Resume.pdf')
+loader = PyPDFLoader('tesla_annual_report.pdf')
 # Split pages from pdf
-pages = loader.load_and_split()
+pages = loader.load_and_split(embedding_function=OpenAIEmbeddings())
 # Load documents into vector database (ChromaDB)
-store = Chroma.from_documents(pages, collection_name='FullTime_Resume')
+store = Chroma.from_documents(pages, collection_name='tesla_annual_report')
+# Create vectorstore info object (works as metadata repo)
+vectorstore_info = VectorStoreInfo(
+    name = "tesla_annual_report",
+    description = "financial report on fiscal year for tesla 2022",
+    vectorstore = store
+)
 
+# Convert document store into vector toolkit
+toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+
+# Add the toolkit to an end-to-end LC (gives our model access to pdf)
+agent_executor = create_vectorstore_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True 
+)
 
 # If the user hits enter 
 if prompt:
-    response = llm(prompt)
+    # response = llm(prompt)
+    respone = agent_executor.run(prompt)
     st.write(response)
+
+    # With a streamlit expander
+    with st.expander('Document Similarity Search'):
+        # searches for relevant passages based on input from user
+        search = store.similarity_search_with_score(prompt)
+        st.write(search[0][0].page_content)
+
+
